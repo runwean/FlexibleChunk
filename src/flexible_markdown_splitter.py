d28aab5dc6dad4d06ff6dc2dd4e5525c07fc948b
runwean <15222715829@163.com>
2025-06-08 14:53:47 +0800

    feat: optimize splitter

---
+++ src/flexible_markdown_splitter.py
@@ -0,0 +1,14 @@
+# -*- coding: utf-8 -*-
+# @Time: 2024/12/01
+
+from mistletoe import Document
+
+
+
+Class SplitException(Exception):
+    pass
+
+def _extract_text_from_tag(tag: "Tag") -> str:
+    pass
+
+
